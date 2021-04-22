dab5ee8a0590367a3c6a4120dd5dcf97a7233e82
fabiopisa <pisanellifabio@gmail.com>
2021-04-22 15:40:16 +0200

    commit esercizio finito con mia variante

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
 
-  var arrList = [];
+  $('input').keyup(function(event){
+    if(event.which === 13){
+      //this è event
+      var item = $(this).val().trim();
 
-  for(var i = 0; i < arrList.length; i++){
-    var item = arrList[i];
-    console.log(item);
+      if(item.length > 3){
+        var strItem = $('.template li').clone();
+        $(strItem).find('.text').append(item);
+        $('main ul').append(strItem);
+        $(this).val('');
+      }
+    }
+  });
 
-    $('input').keyup(function(event){
-      console.log(event.which);
-    });
-    
-  }
+  $(document).on('click', '.fa-trash-alt',function(event){
+    $(this).parent().remove();
+  });
 
 });
